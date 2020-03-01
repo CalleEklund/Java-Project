76b276ee632e9b2f054b2355df2191bc290db6b4
carek123 <carek123@student.liu.se>
2020-03-01 17:57:24 +0100

    en fungerade skriv funktion som kan lägga till användare oavsett hur många gånger createaccount pagen nås

--- src/test.java
+++ src/test.java
@@ -2,7 +2,11 @@ import classes.Loan;
 import classes.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import texthandlers.TextWriter;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,57 +16,92 @@ import java.time.LocalDate;
 import java.util.HashMap;
 
 
-public class test
+import classes.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import texthandlers.TextWriter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class test extends JPanel
 {
+    private TextWriter tw;
+    private HashMap<String, User> userData;
+    private Gson gson;
+    private FileWriter primaryWriter;
+    private FileReader primaryReader;
+
+    JFrame frame = new JFrame();
+    JPanel cont = new JPanel();
+
+    private JTextField name = new JTextField(15);
+    private JTextField email = new JTextField(15);
+    private JTextField password = new JTextField(15);
+
+    private JButton add = new JButton("add user");
+    private JButton logout = new JButton("logout");
+
+    public test() throws IOException {
+	primaryReader = new FileReader(new File("usersData.json"));
+	gson = new GsonBuilder().setPrettyPrinting().create();
+	userData = new HashMap<>();
+	tw = new TextWriter();
+
+	setLayout(new FlowLayout());
+	cont.add(name);
+	cont.add(email);
+	cont.add(password);
+	cont.add(add);
+	cont.add(logout);
+	Action createAcc = new AbstractAction()
+	{
+	    @Override public void actionPerformed(ActionEvent actionEvent) {
+		System.out.println(name.getText() + " " + email.getText() + " " + password.getText());
+		User u = new User(name.getText(), email.getText(), password.getText());
+		HashMap<String, User> temp = gson.fromJson(primaryReader, HashMap.class);
+		if (temp != null){
+		    temp.put(u.getUid(), u);
+		    userData = temp;
+		}
+		userData.put(u.getUid(), u);
+		try {
+		    primaryWriter = new FileWriter(new File("usersData.json"));
+		    primaryWriter.write(gson.toJson(userData));
+		    primaryWriter.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}; Action out = new AbstractAction()
+	{
+	    @Override public void actionPerformed(ActionEvent actionEvent) {
+
+
+	    }
+	};
+	logout.addActionListener(out);
+	add.addActionListener(createAcc);
+
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(cont);
+	frame.setSize(600, 600);
+	frame.pack();
+	frame.setVisible(true);
+    }
+
     /**
-     * TODO:
-     *  - Radera "konton" med att en temp sträng, ta bort användare, lägg till den nya användaren.
-     *
-     * **/
-    public static void main(String[] args) throws FileNotFoundException {
-	HashMap<String, User> data = new HashMap<>();
-	Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-
-	User u1 = new User("Calle", "test1@gmail.com", "losen");
-	User u2 = new User("Tyra", "test2@gmail.com", "losen");
-	u1.addUserLoanshm(new Loan("test1","testdesc1testdesc1testdesc1",2, 1000, 100, LocalDate.of(2000, 10, 10), LocalDate.now()));
-	u2.addUserLoanshm(new Loan("test2", "testdesc2", 2, 1000, 100, LocalDate.of(2000, 10, 10), LocalDate.now()));
-
-	data.put(u1.getEmail(),u1);
-	data.put(u2.getEmail(),u2);
-	System.out.println(gson.toJson(data));
-
-
-	File file = new File("src/usersData.json");
-	FileWriter fw = null;
-	FileReader fr = new FileReader(file);
-	FileWriter fclean = null;
-//	try {
-//	    fr = new FileReader(file);
-//	    fw = new FileWriter(file, true);
-//	    //rensa filen
-//	    fclean = new FileWriter(file);
-////	    fclean.write("");
-//	    fw.write(gson.toJson(data));
-//
-//	} catch (IOException e) {
-//
-//	} finally {
-//	    try {
-//		assert fw != null;
-//		fw.close();
-//	    } catch (IOException e) {
-//		e.printStackTrace();
-//	    }
-//	}
-
-
-	HashMap outdata = gson.fromJson(fr, HashMap.class);
-
-	System.out.println("OUT"+gson.toJson(outdata));
-
-
+     * TODO: - Radera "konton" med att en temp sträng, ta bort användare, lägg till den nya användaren.
+     **/
+    public static void main(String[] args) throws IOException {
+	new test();
 
 
     }
